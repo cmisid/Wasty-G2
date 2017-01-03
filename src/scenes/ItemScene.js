8604b8ff2fae2446685e7cd44d10d8c54d9b9624
Elise Benois <elise-18@hotmail.fr>
2017-01-03 17:32:11 +0100

    Ajout de l’image sur ItemScene

--- src/scenes/ItemScene.js
+++ src/scenes/ItemScene.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 
+import { View, StyleSheet, Dimensions } from 'react-native'
+
+import ProgressiveImage from '../components/ProgressiveImage'
+
 import AppText from '../components/AppText'
 import Container from '../components/Container'
 
@@ -7,7 +11,16 @@ export default class ItemScene extends Component {
   render () {
     return (
       <Container>
-        <AppText>{this.props.item.title}</AppText>
+        <View>
+          <AppText>
+            {this.props.item.title}
+          </AppText> 
+          <ProgressiveImage
+            thumbnailSource={{ uri: this.props.item.imgPlaceholderUrl }}
+            imageSource={{ uri: this.props.item.imgUrl }}
+            style={styles.image}
+          />
+        </View>
       </Container>
     )
   }
@@ -16,3 +29,15 @@ export default class ItemScene extends Component {
 ItemScene.propTypes = {
   item: React.PropTypes.object
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: Dimensions.get('window').width - 10,
+    height: Dimensions.get('window').height / 2 - 10,
+    justifyContent: 'center',
+    alignSelf: 'center'
+  },
+  header: {
+
+  }
+})
