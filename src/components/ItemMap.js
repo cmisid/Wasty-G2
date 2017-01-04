29e890db6f27c8107c70551f82d9d6b2c0b02c3b
RAPH\Raphael <raphael.sourty@gmail.com>
2017-01-04 17:31:05 +0100

    Set up utils fonctions

--- src/components/ItemMap.js
+++ src/components/ItemMap.js
@@ -9,26 +9,7 @@ import Card from './card/Card'
 import CardFooter from './card/CardFooter'
 import ProgressiveImage from './ProgressiveImage'
 
-// Google Maps
-const generateMapLink = (sourceLat, sourceLon, destLat, destLon) => (
-  `http://maps.google.com/maps?saddr=${sourceLat},${sourceLon}&daddr=${destLat},${destLon}`
-)
-
-const toRad = x => x * Math.PI / 180
-
-const haversineDistance = (lat1, lon1, lat2, lon2) => {
-  const dLat = toRad(lat2 - lat1)
-  const dLon = toRad(lon2 - lon1)
-
-  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(toRad(lat1)) *
-        Math.cos(toRad(lat2)) *
-        Math.sin(dLon / 2) * Math.sin(dLon / 2)
-
-  return 12742 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
-}
-
-const distanceFmt = dist => dist < 1 ? `${Math.round((dist * 1000).toFixed(2), 1)} m` : `${Math.round(dist.toFixed(2), 1)} km`
+import {generateMapLink, haversineDistance, distanceFmt, toRad} from './../util.js'
 
 export default class ItemMap extends Component {
   render () {
---
+++ src/util.js
@@ -0,0 +1,20 @@
+//Functions which are used in wasty/src/components/itemCard.js & itemMap.js & itemRow.js
+
+export const generateMapLink = (sourceLat, sourceLon, destLat, destLon) => (
+  `http://maps.google.com/maps?saddr=${sourceLat},${sourceLon}&daddr=${destLat},${destLon}`
+)
+
+export const haversineDistance = (lat1, lon1, lat2, lon2) => {
+  const dLat = toRad(lat2 - lat1)
+  const dLon = toRad(lon2 - lon1)
+  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        	Math.cos(toRad(lat1)) *
+        	Math.cos(toRad(lat2)) *
+        	Math.sin(dLon / 2) * Math.sin(dLon / 2)
+
+  return 12742 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+}
+
+export const distanceFmt = dist => dist < 1 ? `${Math.round((dist * 1000).toFixed(2), 1)} m` : `${Math.round(dist.toFixed(2), 1)} km`
+
+export const toRad = x => x * Math.PI / 180
