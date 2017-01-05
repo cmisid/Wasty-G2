d2c50870fbe310503d016e26d61acdee2fa1129b
RAPH\Raphael <raphael.sourty@gmail.com>
2017-01-05 09:21:07 +0100

    Import function in ItemScene rather than declare it

--- src/scenes/ItemScene.js
+++ src/scenes/ItemScene.js
@@ -7,21 +7,7 @@ import Icon from 'react-native-vector-icons/MaterialIcons'
 import AppText from '../components/AppText'
 import Container from '../components/Container'
 
-const toRad = x => x * Math.PI / 180
-
-const haversineDistance = (lat1, lon1, lat2, lon2) => {
-  const dLat = toRad(lat2 - lat1)
-  const dLon = toRad(lon2 - lon1)
-
-  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(toRad(lat1)) *
-        Math.cos(toRad(lat2)) *
-        Math.sin(dLon / 2) * Math.sin(dLon / 2)
-
-  return 12742 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
-}
-
-const distanceFmt = dist => dist < 1 ? `${Math.round((dist * 1000).toFixed(2), 1)} m` : `${Math.round(dist.toFixed(2), 1)} km`
+import {generateMapLink, haversineDistance, distanceFmt, toRad} from './../util.js'
 
 export default class ItemScene extends Component {
   render () {
