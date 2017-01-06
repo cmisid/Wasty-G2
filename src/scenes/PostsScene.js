0488fd457d0022e59e4751203a4f73faf9faa0e3
AntoinePlissonneau <plissonneau.antoine@gmail.com>
2017-01-06 15:12:26 +0100

    Ajout bouton modal

--- src/scenes/PostsScene.js
+++ src/scenes/PostsScene.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { ListView, StyleSheet, View, Text, RefreshControl } from 'react-native'
+import { ListView, StyleSheet, View, Text, RefreshControl, Dimensions } from 'react-native'
 
 import Modal from 'react-native-modalbox'
 import ActionButton from 'react-native-action-button'
@@ -12,7 +12,7 @@ import { getPosts } from '../store/api'
 import { colors } from '../style'
 
 const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
-
+const width = Dimensions.get('window').width - 30
 export default class PostedScene extends Component {
 
   constructor (props) {
@@ -71,15 +71,17 @@ export default class PostedScene extends Component {
           enableEmptySections
         />
 
-        <Modal style={{height: 200, borderRadius: 5, marginLeft: 0, marginRight: 5}} ref={'modal'}>
+        <Modal style={{height: 200, borderRadius: 5, width: width}} color={'blue'} ref={'modal'} backdropColor={'black'} backdropOpacity={0.3}>
           <Text
             style={{textAlign: 'center', marginLeft: 8, marginRight: 8, marginTop: 10}}
           >Thierry dit avoir récupéré votre {this.state.selectedItem.title}. Voulez-vous confirmer ?</Text>
-          <ActionButton
-            buttonColor={colors.primary}
-            icon={<Icon color='white' name='md-done-all' size={20} />}
-            onPress={() => Actions.searchPostItemScene()}
-          />
+          <View style={{position: 'absolute', marginTop: 135, marginLeft: 270}}>
+            <ActionButton
+              buttonColor={colors.primary}
+              icon={<Icon color='white' name='check' size={20} />}
+              onPress={() => Actions.searchPostItemScene()}
+            />
+          </View>
         </Modal>
       </Container>
     )
