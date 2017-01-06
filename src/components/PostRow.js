60293663628b7ab55b836eb4b32387c6413bcdf1
Elise Benois <elise-18@hotmail.fr>
2017-01-06 09:31:25 +0100

    Modification code

--- src/components/PostRow.js
+++ src/components/PostRow.js
@@ -17,74 +17,61 @@ export default class ItemRow extends Component {
       <TouchableHighlight onPress={this.props.onPressAction}>
         <View style={{flex: 1}}>
           <Card>
-            <View>
-              <Icon
-                name={this.props.item.pickedUp ? 'check' : ''}
-                iconStyle={{textAlign: 'center'}}
-                size={20}
-                color='green'
-                style={{textAlign: 'center'}}
+            <View
+              style={styles.row}
+            >
+              <ProgressiveImage
+                thumbnailSource={{ uri: this.props.item.imgPlaceholderUrl }}
+                imageSource={{ uri: this.props.item.imgUrl }}
+                style={styles.image}
               />
               <View
-                style={styles.row}
+                style={{flex: 2, marginLeft: 5}}
               >
-                <ProgressiveImage
-                  thumbnailSource={{ uri: this.props.item.imgPlaceholderUrl }}
-                  imageSource={{ uri: this.props.item.imgUrl }}
-                  style={styles.image}
-                />
-                <View
-                  style={{flex: 2, marginLeft: 5}}
+                <AppText
+                  style={StyleSheet.flatten(styles.title)}
                 >
-                  <AppText
-                    style={StyleSheet.flatten(styles.title)}
-                  >
-                    {this.props.item.title} publié {distanceInWordsToNow(
-                        this.props.item.publishDate,
-                        {locale: frLocale, addSuffix: true}
-                      )}
-                  </AppText>
-                  <AppText
-                    style={StyleSheet.flatten(styles.category)}
-                  >
-                    {this.props.item.category}
-                  </AppText>
-                  <View
-                    style={styles.content}
-                  >
-                    <View style={{flexDirection: 'row', marginRight: 5, marginLeft: 200, marginBottom: 8}}>
-                      <Icon
-                        name='star'
+                  {this.props.item.title} publié {distanceInWordsToNow(
+                      this.props.item.publishDate,
+                      {locale: frLocale, addSuffix: true}
+                    )}
+                </AppText>
+                <AppText
+                  style={StyleSheet.flatten(styles.category)}
+                >
+                  {this.props.item.category}
+                </AppText>
+                <View
+                  style={styles.content}
+                >
+                  <View style={{flexDirection: 'row', marginRight: 5, marginLeft: 200, marginBottom: 8}}>
+                    <Icon
+                      name='star'
 
-                        iconStyle={{marginTop: 10}}
-                        size={20}
-                        color='gold'
-                      />
+                      iconStyle={{marginTop: 10}}
+                      size={20}
+                      color='gold'
+                    />
 
-                      <AppText> {this.props.item.nLikes}</AppText>
-                    </View>
-                  </View>
-                  <View
-                    style={styles.content}
-                  >
-                    <View style={{flexDirection: 'row', marginRight: 5, marginLeft: 200, marginBottom: 5}}>
-                      <Icon name='remove-red-eye' iconStyle={{marginTop: 10}} size={20} color={colors.secondary} />
-                      <AppText> {this.props.item.nViews}</AppText>
-                    </View>
+                    <AppText> {this.props.item.nLikes}</AppText>
                   </View>
                 </View>
-                
-                
-                
-                {this.props.item.pickedUp &&
-                  <View style={this.props.item.pickedUp && styles.overlay} >
-                    <View style={{flexDirection: 'row', marginRight: 5, marginLeft: 150, marginBottom: 5}}>
-                      <Icon name='done' iconStyle={{marginTop: 10}} size={100} color='darkgreen' />
-                    </View>
-                
+                <View
+                  style={styles.content}
+                >
+                  <View style={{flexDirection: 'row', marginRight: 5, marginLeft: 200, marginBottom: 5}}>
+                    <Icon name='remove-red-eye' iconStyle={{marginTop: 10}} size={20} color={colors.secondary} />
+                    <AppText> {this.props.item.nViews}</AppText>
+                  </View>
+                </View>
+              </View>
+              {this.props.item.pickedUp &&
+                <View style={styles.overlay} >
+                  <View style={{flexDirection: 'row', marginRight: 5, marginLeft: 150, marginBottom: 5}}>
+                    <Icon name='done' iconStyle={{marginTop: 10}} size={100} color='darkgreen' />
+                  </View>
                 </View>
               }
-              </View>
             </View>
           </Card>
         </View>
