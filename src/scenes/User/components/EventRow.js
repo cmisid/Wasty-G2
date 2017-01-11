42beaba50ae6e0e78d500e72e656e4d00a746d7a
Max Halford <maxhalford25@gmail.com>
2017-01-11 09:10:11 +0100

    Add item view from event

--- src/scenes/User/components/EventRow.js
+++ src/scenes/User/components/EventRow.js
@@ -3,6 +3,7 @@ import { StyleSheet, View } from 'react-native'
 
 import distanceInWordsToNow from 'date-fns/distance_in_words_to_now'
 import frLocale from 'date-fns/locale/fr'
+import { Actions } from 'react-native-router-flux'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import AppText from '../../../components/AppText'
@@ -21,7 +22,7 @@ export default class EventRow extends Component {
           />
         </View>
         <View style={{flex: 5}}>
-          <AppText>
+          <AppText onPress={() => Actions.accountItemScene({item: this.props.event.item})}>
             {`"${this.props.event.item.title}" ${this.props.event.action === 'post' ? 'posté' : 'récupéré'} ${distanceInWordsToNow(
                   this.props.event.date,
                   {locale: frLocale, addSuffix: true}
