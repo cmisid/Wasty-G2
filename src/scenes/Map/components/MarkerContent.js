4612b9c33ef7c020bcfc2d7b1bd7f61fef2a647f
Axel Bellec <axel.bellec@outlook.fr>
2017-01-11 10:31:39 +0100

    init MarkerContent

--- src/scenes/Map/components/MarkerContent.js
+++ src/scenes/Map/components/MarkerContent.js
@@ -1,52 +1,111 @@
-import React, { Component } from 'react'
-import { View, StyleSheet, Linking, TouchableHighlight } from 'react-native'
+import React, {Component} from 'react'
+
+import { Dimensions, Linking, StyleSheet, TouchableHighlight, View } from 'react-native'
 
 import Icon from 'react-native-vector-icons/MaterialIcons'
 import Swipeout from 'react-native-swipeout'
+import Toast from 'react-native-root-toast'
+import { Actions } from 'react-native-router-flux'
+import CornerLabel from 'react-native-smart-corner-label'
 
 import AppText from '../../../components/AppText'
 import Card from '../../../components/card/Card'
+import CardHeader from '../../../components/card/CardHeader'
 import ProgressiveImage from '../../../components/ProgressiveImage'
-import { colors } from '../../../style'
+import { colors, zIndexes } from '../../../style'
 
-export default class ItemRow extends Component {
+const toast = text => Toast.show(text, {
+  duration: Toast.durations.LONG,
+  position: Toast.positions.BOTTOM,
+  shadow: true,
+  animation: true,
+  hideOnPress: true,
+  delay: 500,
+  backgroundColor: colors.primary,
+  shadowColor: colors.background,
+  textColor: 'white'
+})
+
+export default class MarkerContent extends Component {
 
   constructor (props) {
     super(props)
-    this.state = {
-      deleteButton: [
-        {
-          text: 'Supprimer',
-          backgroundColor: 'lightcoral',
-          color: 'white',
-          underlayColor: 'dimgray',
-          onPress: () => console.log('Swipe button event')
-        }
-      ]
-    }
+    this.likeButton = [
+      {
+        text: <Icon name='favorite-border' iconStyle={{marginTop: 10}} size={30} color='white' />,
+        backgroundColor: 'orange',
+        color: 'white',
+        underlayColor: 'orange',
+        onPress: () => this._onLeftSwipeoutPressed()
+      }
+    ]
+  }
+
+  _onLeftSwipeoutPressed () {
+    toast(<AppText style={StyleSheet.flatten(styles.toast)}>{`"${this.props.item.title}" a été ajouté à votre liste d'items`}</AppText>)
+    this.props.onLikeItem(this.props.item.id)
   }
 
   render () {
     return (
       <Card>
-        {/* FIXME : corriger le marginTop */}
-        <View style={{height: 100, flex: 1, flexDirection: 'row', margin: 10, marginTop: 5}}>
-
-          <View style={{flex: 2, alignItems: 'center', justifyContent: 'center'}}>
+        {/* Item title and category */}
+        <CardHeader
+          title={this.props.item.title}
+          category={this.props.item.category}
+        />
+        {/* Swipeout to add an item as favorite */}
+        <Swipeout
+          right={this.likeButton}
+          autoClose
+          sensitivity={0.9}
+          style={{backgroundColor: colors.background}}
+        >
+          <View style={{zIndex: zIndexes.background}}>
+            {/* Item image */}
             <ProgressiveImage
               thumbnailSource={{ uri: this.props.item.imgPlaceholderUrl }}
               imageSource={{ uri: this.props.item.imgUrl }}
               style={styles.image}
             />
+            {/* Corner label to display the item price */}
+            <CornerLabel
+              alignment='right'
+              cornerRadius={60}
+              style={{backgroundColor: this.props.item.price > 0.0 ? 'orange' : 'mediumseagreen', borderRadius: 20}}
+              textStyle={{fontSize: 10, color: 'white', fontWeight: 'bold'}}
+            >
+              {this.props.item.price > 0.0 ? `\n${this.props.item.price} €` : '\nGRATUIT'}
+            </CornerLabel>
+          </View>
+        </Swipeout>
+        {/* Metadata footer */}
+        <View style={{height: 80, flex: 1, flexDirection: 'row'}}>
+
+          {/* User image */}
+          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <TouchableHighlight
+              onPress={() => Actions.searchUserScene({user: this.props.item.publisher})}
+              underlayColor={colors.transparent}
+            >
+              <View>
+                <ProgressiveImage
+                  thumbnailSource={{ uri: this.props.item.publisher.imgPlaceholderUrl }}
+                  imageSource={{ uri: this.props.item.publisher.imgUrl }}
+                  style={{width: 40, height: 40, borderRadius: 20}}
+                />
+              </View>
+            </TouchableHighlight>
           </View>
 
-          <View style={{flex: 4, justifyContent: 'center', paddingLeft: 5}}>
-            <AppText style={{fontWeight: 'bold'}}>
-              {this.props.item.title}
-            </AppText>
-            <AppText style={{color: 'grey'}}>
-              {this.props.item.category}
-            </AppText>
+          {/* Item essential information */}
+          <View style={{flex: 4, justifyContent: 'center'}}>
+            {/* Item's publisher full name */}
+            <View style={{flexDirection: 'row'}}>
+              <AppText>{this.props.item.publisher.fullName}</AppText>
+              <AppText style={{color: colors.background}}> {this.props.item.readablePublishedSince}</AppText>
+            </View>
+            {/* Item's address */}
             <AppText
               onPress={() => Linking.openURL(this.props.item.address.generateMapLink(
                 this.props.userLat,
@@ -79,36 +138,21 @@ export default class ItemRow extends Component {
 }
 
 const styles = StyleSheet.create({
-  content: {
-    flex: 1,
-    marginLeft: 10,
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    justifyContent: 'space-between'
+  toast: {
+    fontWeight: 'bold'
   },
   image: {
-    width: 100 - 10,
-    height: 100 - 10,
-    marginTop: 5,
-    marginBottom: 10,
-    borderRadius: 5
-  },
-  row: {
-    flex: 1,
-    flexDirection: 'row',
-    alignSelf: 'flex-start',
-    marginBottom: 5,
-    marginLeft: 10
-  },
-  distance: {
-    fontStyle: 'italic',
-    color: colors.background,
-    marginRight: 5
+    width: Dimensions.get('window').width - 10,
+    height: Dimensions.get('window').height / 4 - 10,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    borderRadius: 1
   }
 })
 
-ItemRow.propTypes = {
+MarkerContent.propTypes = {
   item: React.PropTypes.object,
   userLat: React.PropTypes.number,
-  userLon: React.PropTypes.number
+  userLon: React.PropTypes.number,
+  onLikeItem: React.PropTypes.func
 }
