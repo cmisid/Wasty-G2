f73b8fc6622ddc202089bf6f9115366ea5365a90
Elise Benois <elise-18@hotmail.fr>
2017-01-12 09:19:28 +0100

    Correction dans README

--- src/scenes/Posts/index.js
+++ src/scenes/Posts/index.js
@@ -1,10 +1,4 @@
-/* <Text color={'blue'}>
-              {`${this.state.selectedItem.picker.firstName} ${this.state.selectedItem.picker.lastName} `}
-            </Text>
-            <Text>
-              dit avoir récupéré :
-            </Text>
-            There are three documents. ItemRowContent is the style of Post. ItemRow contains older logics.
+/* There are three documents. ItemRowContent is the style of Post. ItemRow contains older logics.
 ItemRowContent and ItemRow are the childs of these Index.js. */
 
 import React, { Component } from 'react'
