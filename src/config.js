a7b337f6ff26a10ade28441608fc82981cd02999
Axel Bellec <axel.bellec@outlook.fr>
2017-01-12 10:29:13 +0100

    update WEB_SERVICES_URLS

--- src/config.js
+++ src/config.js
@@ -2,3 +2,10 @@
 export const appEnv = process.env.APP_ENV || 'dev'
 
 export const isProd = appEnv === 'prod'
+
+// TODO: add the production url
+export const WEB_SERVICES_URLS = {
+  ROUTING_URL: isProd ? 'http://localhost:5000/' : 'http://localhost:5000/',
+  IMG_CLASSIFIER_URL: isProd ? 'http://localhost:5000/' : 'http://localhost:5000/'
+}
+
